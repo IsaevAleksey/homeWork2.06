a4843b75cc799f8dc79feb452827c27c9b38cb4e
Alexey Isaev <montilarard@gmail.com>
2022-11-21 20:44:26 +0300

    fix

--- homeWork2.6/StartViewController.swift
+++ homeWork2.6/StartViewController.swift
@@ -12,16 +12,16 @@ final class StartViewController: UIViewController {
     @IBOutlet var usernameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
-    private var login = "User"
+    private var user = "User"
     private var password = "Password"
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let userVC = segue.destination as? UserViewController {
+        guard let userVC = segue.destination as? UserViewController else { return }
             userVC.user = usernameTF.text
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
     
@@ -31,28 +31,26 @@ final class StartViewController: UIViewController {
     }
     
     @IBAction func loginActionButton() {
-        guard usernameTF.text == login, passwordTF.text == password else {
-            showAlert(with: "Please, try again", and: "Incorrect username or password")
+        guard usernameTF.text == user, passwordTF.text == password else {
+            showAlert(with: "Please, try again", and: "Incorrect username or password", textField: passwordTF)
             return
         }
+        performSegue(withIdentifier: "showUserVC", sender: nil)
     }
     
-    @IBAction func remindUsername() {
-        showAlert(with: "Username", and: "Your name is User")
-    }
     
-    @IBAction func remindPassword() {
-        showAlert(with: "Password", and: "Your password is Password")
+    @IBAction func remindUserData(_ sender: UIButton ) {
+        sender.tag == 0
+        ? showAlert(with: "Username", and: "Your name is \(user)")
+        : showAlert(with: "Password", and: "Your password is \(password)")
     }
-    
-
 }
 
 extension StartViewController {
-    private func showAlert(with title: String, and massage: String) {
-        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
-            self.passwordTF.text = ""
+    private func showAlert(with title: String, and message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
         }
         alert.addAction(okAction)
         present(alert, animated: true)
